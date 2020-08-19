4258bb0e63c835ad8f52f32fc015e0052a61a42a
coderarjob <mukherjeearjob@gmail.com>
2020-08-19 16:45:47 +0530

    feature-multiple-input complete

--- main.c
+++ main.c
@@ -9,46 +9,46 @@
  *   - https://en.wikipedia.org/wiki/Tiny_Encryption_Algorithm
  *   - http://www.tayloredge.com/reference/Mathematics/TEA-XTEA.pdf
  *
- *	Author: Arjob Mukherjee (arjobmukherjee@gmail.com)
- *	Dated : 16th August 2020
+ *    Author: Arjob Mukherjee (arjobmukherjee@gmail.com)
+ *    Dated : 16th August 2020
  * ---------------------------------------------------------------------------
  * */
-#include <stdio.h>	    // For printf and NULL, etc..
-#include <stdint.h>	    // For standard int types
+#include <stdio.h>        // For printf and NULL, etc..
+#include <stdint.h>        // For standard int types
 #include <stdlib.h>     // For exit, malloc
 #include <string.h>     // For strcmp, strlen, etc..
 #include "tea.h"
 
 #define USAGE(p) fprintf(stderr, \
-		"Tiny Encryption Algorithm implementation, with 128 bit key.\n" \
-		"Performs Encryption/Decruption of multiple flies.\n" \
-		"usage:\n%s [-e|-d] -k '16 byte key' -I <...>\n" \
-		"-e    - Encrypt\n" \
-		"        Encrypts the input files and the output files of each" \
-		" will be placed in the same directory with extension .3\n" \
-		"-d    - Decrypt\n" \
-		"        Decrypts the input files and the output files of each" \
-		" will be placed in the same directory excluding extension .3\n" \
-		"-k    - 16 byte key.\n" \
-		"-I    - Files that need to be processed.\n" \
-		, p)
+        "Tiny Encryption Algorithm implementation, with 128 bit key.\n" \
+        "Performs Encryption/Decruption of multiple flies.\n" \
+        "usage:\n%s [-e|-d] -k '16 byte key' -I <...>\n" \
+        "-e    - Encrypt\n" \
+        "        Encrypts the input files and the output files of each" \
+        " will be placed in the same directory with extension .3\n" \
+        "-d    - Decrypt\n" \
+        "        Decrypts the input files and the output files of each" \
+        " will be placed in the same directory excluding extension .3\n" \
+        "-k    - 16 byte key.\n" \
+        "-I    - Files that need to be processed.\n" \
+        , p)
 
 #define MAX_FILENAME_LENGTH 255 // Length of file path
 #define MAX_INPUT_FILES 50 // number of files
 #define ENCRYPTED_FILE_EXTENSION ".3"
 
 enum errors { 
-				ERR_NONE, 
-	         	ERR_MALLOC, 
-	         	ERR_FILE_FAILED, 
-	         	ERR_INVALID_ARG
-			};
+                ERR_NONE, 
+                 ERR_MALLOC, 
+                 ERR_FILE_FAILED, 
+                 ERR_INVALID_ARG
+            };
 struct op
 {
-	int mode;
-	int count;
-	char key[KEY_SIZE];
-	char *files[MAX_INPUT_FILES];
+    int mode;
+    int count;
+    char key[KEY_SIZE];
+    char *files[MAX_INPUT_FILES];
 };
 
 int readargs(char *argv[], struct op *out);
@@ -58,53 +58,53 @@ int strip_extension(char *filename, char *extension, char *out);
 
 int main(int argc,char *argv[])
 {
-	struct op prm = {0};
+    struct op prm = {0};
 
-	// 1. Read parameters
-	if (argc >= 4)
-		readargs(argv,&prm);
+    // 1. Read parameters
+    if (argc >= 4)
+        readargs(argv,&prm);
 
-	if (prm.mode == UNSET || prm.key[0] == 0)
-	{
-		USAGE(argv[0]);
-		exit(ERR_INVALID_ARG);
-	}
+    if (prm.mode == UNSET || prm.key[0] == 0)
+    {
+        USAGE(argv[0]);
+        exit(ERR_INVALID_ARG);
+    }
 
-	// Now we encrypt/decrypt each of the files.
-	char output_filename[MAX_FILENAME_LENGTH + 2];
+    // Now we encrypt/decrypt each of the files.
+    char output_filename[MAX_FILENAME_LENGTH + 2];
 
-	for(int i = 0; i < prm.count; i++) {
+    for(int i = 0; i < prm.count; i++) {
 
-		if (prm.mode == ENCRYPT) {
-			// Output file name: InputfilePath + ".3"
-			output_filename[0] = '\0';
-			strcat(output_filename, prm.files[i]);
-			strcat(output_filename, ENCRYPTED_FILE_EXTENSION);
+        if (prm.mode == ENCRYPT) {
+            // Output file name: InputfilePath + ".3"
+            output_filename[0] = '\0';
+            strcat(output_filename, prm.files[i]);
+            strcat(output_filename, ENCRYPTED_FILE_EXTENSION);
 
-			encrypt_decrypt (ENCRYPT, 
-					         prm.files[i], 
-							 output_filename, 
-							 prm.key);
-		}
-		else{
-			// Output file name: InputFilePath - ".3" extension
-			if (strip_extension(prm.files[i], 
-					        ENCRYPTED_FILE_EXTENSION,
-					        output_filename) == ERR_INVALID_ARG){
-				fprintf(stderr,"Warning: Invalid file extension in %s\n",
-						prm.files[i]);
-				continue;
-			}
-			
+            encrypt_decrypt (ENCRYPT, 
+                             prm.files[i], 
+                             output_filename, 
+                             prm.key);
+        }
+        else{
+            // Output file name: InputFilePath - ".3" extension
+            if (strip_extension(prm.files[i], 
+                            ENCRYPTED_FILE_EXTENSION,
+                            output_filename) == ERR_INVALID_ARG){
+                fprintf(stderr,"Warning: Invalid file extension in %s\n",
+                        prm.files[i]);
+                continue;
+            }
+            
 
-			encrypt_decrypt (DECRYPT, 
-					         prm.files[i], 
-							 output_filename, 
-							 prm.key);
-		}
-	}
+            encrypt_decrypt (DECRYPT, 
+                             prm.files[i], 
+                             output_filename, 
+                             prm.key);
+        }
+    }
 
-	return ERR_NONE;
+    return ERR_NONE;
 }
 
 
@@ -114,16 +114,16 @@ int main(int argc,char *argv[])
  * */
 int strip_extension(char *filename, char *extension, char *out)
 {
-	int flen = strlen(filename),
-		el    = flen - strlen(extension);   // Extension begins at this index.
+    int flen = strlen(filename),
+        el    = flen - strlen(extension);   // Extension begins at this index.
 
-	*out = '\0';
-	if (strcmp (filename + el,extension))
-		return ERR_INVALID_ARG;
+    *out = '\0';
+    if (strcmp (filename + el,extension))
+        return ERR_INVALID_ARG;
 
-	strcpy(out, filename);
-	*(out + el) = '\0';
-	return ERR_NONE;
+    strcpy(out, filename);
+    *(out + el) = '\0';
+    return ERR_NONE;
 }
 
 /*
@@ -132,93 +132,93 @@ int strip_extension(char *filename, char *extension, char *out)
  * */
 int readargs(char *argv[], struct op *out)
 {
-	char *arg;
+    char *arg;
 
-	// Default options
-	out->mode = UNSET;
-	out->count = 0;
+    // Default options
+    out->mode = UNSET;
+    out->count = 0;
 
-	while((arg = *argv++)) {
-		if (*arg == '-') {
-			while(*++arg){
-				switch(*arg) {
-					case 'e':
-						out->mode = ENCRYPT;
-						break;
-					case 'd':
-						out->mode = DECRYPT;
-						break;
-					case 'k':
-						read_args_key(*argv++,out);
-						break;
-					case 'I':
-						argv = read_args_files(argv,out);
-						break;
-					default:
-						fprintf(stderr,"Error: Invaid argument: %s\n",arg);
-						return ERR_INVALID_ARG;
-						break;
-				}
-			}
-		}
-	}
-	return 0;
+    while((arg = *argv++)) {
+        if (*arg == '-') {
+            while(*++arg){
+                switch(*arg) {
+                    case 'e':
+                        out->mode = ENCRYPT;
+                        break;
+                    case 'd':
+                        out->mode = DECRYPT;
+                        break;
+                    case 'k':
+                        read_args_key(*argv++,out);
+                        break;
+                    case 'I':
+                        argv = read_args_files(argv,out);
+                        break;
+                    default:
+                        fprintf(stderr,"Error: Invaid argument: %s\n",arg);
+                        return ERR_INVALID_ARG;
+                        break;
+                }
+            }
+        }
+    }
+    return 0;
 }
 
 int read_args_key(char *arg, struct op *out)
 {
-	if (strlen(arg) != KEY_SIZE){
-		fprintf(stderr,
-				"Error: Invalid key. Must be %u bytes long.\n", KEY_SIZE);
-		return ERR_INVALID_ARG;
-	}
+    if (strlen(arg) != KEY_SIZE){
+        fprintf(stderr,
+                "Error: Invalid key. Must be %u bytes long.\n", KEY_SIZE);
+        return ERR_INVALID_ARG;
+    }
 
-	memcpy(out->key,arg,KEY_SIZE);	
-	return 0;
+    memcpy(out->key,arg,KEY_SIZE);    
+    return 0;
 }
 
 char **read_args_files(char *argv[], struct op *out)
 {
-	int filei;		// Points to the string in argv being copied.
-	char *file;		// Points to the current string at argv[filei]
+    int filei;        // Points to the string in argv being copied.
+    char *file;        // Points to the current string at argv[filei]
 
-	// It will take each of the strings in argv and copy them to out->files[].
-	// The loop stops when there are no more strings in argv array or when the
-	// current pointed string starts with a - (- marks start of parameter).
-	for(filei = 0 
-			; (file = argv[filei]) && *file != '-' && filei < MAX_INPUT_FILES 
-			; filei++)
-	{
+    // It will take each of the strings in argv and copy them to out->files[].
+    // The loop stops when there are no more strings in argv array or when the
+    // current pointed string starts with a - (- marks start of parameter).
+    for(filei = 0 
+            ; (file = argv[filei]) && *file != '-' && filei < MAX_INPUT_FILES 
+            ; filei++)
+    {
 
 
-		// Check the length of the file name.
-		// Check if filename + EOL < MAX_FILENAME_LENGTH
+        // Check the length of the file name.
+        // Check if filename + EOL < MAX_FILENAME_LENGTH
 
-		int len = strlen(file) + 1;
-		if (len > MAX_FILENAME_LENGTH) {
-			fprintf(stderr,"Warning: Skipping, too long: %s", file);
-			continue;
-		}
+        int len = strlen(file) + 1;
+        if (len > MAX_FILENAME_LENGTH) {
+            fprintf(stderr,"Warning: Skipping, too long: %s", file);
+            continue;
+        }
 
-		// Create a string if required length.
+        // Create a string if required length.
 
-		if ((out->files[filei] = malloc(sizeof(char) * len)) == NULL) {
-			perror("malloc");
-			exit(ERR_MALLOC);
-		}
+        if ((out->files[filei] = malloc(sizeof(char) * len)) == NULL) {
+            perror("malloc");
+            exit(ERR_MALLOC);
+        }
 
-		// Copy file path from argument to file->files[filei]
+        // Copy file path from argument to file->files[filei]
 
-		memcpy(out->files[filei],file,len);	
-	}
+        memcpy(out->files[filei],file,len);    
+    }
 
-	// There are more files than space in out->files[] i.e > MAX_INPUT_FILES
+    // There are more files than space in out->files[] i.e > MAX_INPUT_FILES
 
-	if (file != NULL && *file != '-')
-		fprintf(stderr, "Warning: Too many files. Skipping after: %s\n",file);
+    if (file != NULL && *file != '-')
+        fprintf(stderr, "Warning: Too many files. Skipping after: %s\n",file);
 
-	// Update count
-	out->count = filei;
+    // Update count
+    out->count = filei;
 
-	return &argv[filei];
+    return &argv[filei];
 }
--- tea.h
+++ tea.h
@@ -5,30 +5,30 @@
  *  Encrypts/Decrypts reading from input stream, writes back to output
  *  stream.
  *
- *	Author: Arjob Mukherjee (arjobmukherjee@gmail.com)
- *	Dated : 16th August 2020
+ *    Author: Arjob Mukherjee (arjobmukherjee@gmail.com)
+ *    Dated : 16th August 2020
  * ---------------------------------------------------------------------------
  * */
 #ifndef __TEA__
 #define __TEA__
 
-	#include <stdint.h>	// For standard int types
-	#include <stdbool.h>	// for bool, true, false
+    #include <stdint.h>    // For standard int types
+    #include <stdbool.h>    // for bool, true, false
 
-	// Cannot be changed, without impact to the code.
-	
-	#define KEY_SIZE 16 	// bytes
-	#define DATA_SIZE 8		// bytes
+    // Cannot be changed, without impact to the code.
+    
+    #define KEY_SIZE 16     // bytes
+    #define DATA_SIZE 8        // bytes
 
-	// Can be changed if needed.
-	
-	#define DEFAULT_FILE_CREATION_MODE	00644
+    // Can be changed if needed.
+    
+    #define DEFAULT_FILE_CREATION_MODE    00644
 
-	enum opmode {UNSET,
-			     ENCRYPT, 
-				 DECRYPT};
+    enum opmode {UNSET,
+                 ENCRYPT, 
+                 DECRYPT};
 
-	void code(uint32_t v[1], uint32_t k[3]);
-	void decode(uint32_t v[2], uint32_t k[4]);
-	bool encrypt_decrypt(int mode, char *in_file, char *out_file, char *key);
+    void code(uint32_t v[1], uint32_t k[3]);
+    void decode(uint32_t v[2], uint32_t k[4]);
+    bool encrypt_decrypt(int mode, char *in_file, char *out_file, char *key);
 #endif // __TEA__
