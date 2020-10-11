5b876e506532680878ca68bc34e144226cd285e8
arjobwin <mukherjeearjob@gmail.com>
2020-10-12 01:31:47 +0800

    Added CRT links and description in the comments

--- crossplatform.h
+++ crossplatform.h
@@ -1,6 +1,17 @@
 /*
  * Hold all the definations and header file includes, that change with
  * platform.
+ * ---------------------------------------------------------------------------
+ * Information:
+ * ---------------------------------------------------------------------------
+ *  WINDOWS:
+ *  The C++ Compiler comes with C Runtime Library which is responsible for
+ *  providing a POSIC like compatability. When compiling a C file with CL, the
+ *  CRT library msvcrt.lib by default. 
+ *  Helpful links:
+ *      - https://docs.microsoft.com/en-us/cpp/c-runtime-library/crt-library-features?view=vs-2019
+ *      - https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/crt-alphabetical-function-reference?view=vs-2019
+ * ---------------------------------------------------------------------------
  * */
 #ifndef __CROSS_PLATFORM__
 #define __CROSS_PLATFORM__
